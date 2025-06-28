a3d4becf4eb0cd7f11dd50ecd1d72850809425e3
Aksharass <113843078+Aksharass@users.noreply.github.com>
2025-06-28 21:50:59 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -78,9 +78,9 @@ const Home = () => {
   ];
 
   return (
-    <div className="">
+    <div className="overflow-x-hidden w-full">
       {/* Carousel Section */}
-      <section id="home" className="relative w-full bg-white pt-8 sm:pt-20 md:pb-0 group px-3 sm:px-0">
+      <section id="home" className="relative w-full bg-white pt-8 sm:pt-20 md:pb-0 group px-3 sm:px-0 overflow-x-hidden">
         {/* Master Heading and Single Animated Standard Point - with background animation */}
         <div className="relative w-full flex flex-col items-center justify-center z-10 overflow-hidden pt-12 pb-10 sm:pt-4 sm:pb-4 md:pt-4 md:pb-4 lg:pt-4 lg:pb-4">
           {/* Animated balls background for Master content */}
